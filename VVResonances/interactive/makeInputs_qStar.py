c96c21f72a7551427e79e8cc026baabf456aa2da
jngadiub <jennifer.ngadiuba@cern.ch>
2017-08-17 21:45:26 +0200

    updated up to the 2D kernel part, slightly changed the main scripts to resolve ambiguities in leg1 and leg2

--- VVResonances/interactive/makeInputs_qStar.py
+++ VVResonances/interactive/makeInputs_qStar.py
@@ -8,6 +8,8 @@ cuts['common'] = '((HLT_JJ)*(run>500) + (run<500))*(njj>0&&Flag_goodVertices&&Fl
 cuts['HP'] = '(jj_l1_tau2/jj_l1_tau1<0.35)'
 cuts['LP'] = '(jj_l1_tau2/jj_l1_tau1>0.35 && jj_l1_tau2/jj_l1_tau1<0.75)'
 
+cuts['nonres'] = '1'
+
 purities=['HP','LP']
 
 qWTemplate="QstarToQW"
@@ -72,7 +74,39 @@ def makeSignalYields(filename,template,branchingFraction,sfP = {'HP':1.0,'LP':1.
   yieldFile=filename+"_"+p+"_yield"
   cmd='vvMakeSignalYields.py -s {template} -c "{cut}" -o {output} -V "jj_LV_mass" -m {minMVV} -M {maxMVV} -f "pol5" -b {BR} -x 950 samples'.format(template=template, cut=cut, output=yieldFile,minMVV=minMVV,maxMVV=maxMVV,BR=branchingFraction)
   os.system(cmd)
-					
+
+def makeBackgroundShapesMJJ(name,filename,template,addCut="1"):
+
+ #first parameterize detector response
+ cut='*'.join([cuts['common'],'jj_l1_gen_softDrop_mass>10&&jj_gen_partialMass>0',addCut])
+ resFile=filename+"_"+name+"_detectorResponse.root"	       
+ cmd='vvMake2DDetectorParam.py  -o "{rootFile}" -s "{samples}" -c "{cut}"  -v "jj_LV_mass,jj_l1_softDrop_mass"  -g "jj_gen_partialMass,jj_l1_gen_softDrop_mass,jj_l1_gen_pt"  -b "150,200,250,300,350,400,450,500,600,700,800,900,1000,1500,2000,5000"   samples'.format(rootFile=resFile,samples=template,cut=cut,binsMVV=binsMVV,minMVV=minMVV,maxMVV=maxMVV,tag=name)
+ #os.system(cmd)
+ 
+ for p in purities:
+
+  print "=========== PURITY: ", p
+  cut='*'.join([cuts['common'],cuts[p],addCut,cuts['acceptanceGENMJJ']])
+  rootFile=filename+"_"+name+"_MJJ_"+p+".root"  	      
+  cmd='vvMake1DTemplateWithKernels.py -H "y" -o "{rootFile}" -s "{samples}" -c "{cut}"  -v "jj_l1_gen_softDrop_mass" -b {binsMJJ}  -x {minMJJ} -X {maxMJJ} -r {res} samples'.format(rootFile=rootFile,samples=template,cut=cut,res=resFile,binsMJJ=binsMJJ,minMJJ=minMJJ,maxMJJ=maxMJJ)
+  os.system(cmd)
+
+def makeBackgroundShapesMVVConditional(name,filename,template,addCut=""):
+
+ #first parameterize detector response
+ cut='*'.join([cuts['common'],'jj_l1_gen_softDrop_mass>10&&jj_gen_partialMass>0',addCut])
+ resFile=filename+"_"+name+"_detectorResponse.root"	       
+ cmd='vvMake2DDetectorParam.py  -o "{rootFile}" -s "{samples}" -c "{cut}"  -v "jj_LV_mass,jj_l1_softDrop_mass"  -g "jj_gen_partialMass,jj_l1_gen_softDrop_mass,jj_l1_gen_pt"  -b "150,200,250,300,350,400,450,500,600,700,800,900,1000,1500,2000,5000"   samples'.format(rootFile=resFile,samples=template,cut=cut,binsMVV=binsMVV,minMVV=minMVV,maxMVV=maxMVV,tag=name)
+ os.system(cmd)
+
+ for p in purities:
+
+  print "=========== PURITY: ", p
+  cut='*'.join([cuts['common'],cuts[p],addCut,cuts['acceptanceGEN']])
+  rootFile=filename+"_"+name+"_COND2D_"+p+".root"		 
+  cmd='vvMake2DTemplateWithKernels.py  -o "{rootFile}" -s "{samples}" -c "{cut}"  -v "jj_gen_partialMass,jj_l1_gen_softDrop_mass"  -b {binsMVV} -B {binsMJJ} -x {minMVV} -X {maxMVV} -y {minMJJ} -Y {maxMJJ}  -r {res} samples'.format(rootFile=rootFile,samples=template,cut=cut,binsMVV=binsMVV,minMVV=minMVV,maxMVV=maxMVV,res=resFile,binsMJJ=binsMJJ,minMJJ=minMJJ,maxMJJ=maxMJJ)
+  os.system(cmd)
+									
 makeSignalShapesMVV("JJ_XqW",qWTemplate)
 makeSignalShapesMJJ("JJ_XqW",qWTemplate)
 makeSignalYields("JJ_XqW",qWTemplate,BRqW,{'HP':1.03,'LP':0.95})
@@ -80,3 +114,6 @@ makeSignalYields("JJ_XqW",qWTemplate,BRqW,{'HP':1.03,'LP':0.95})
 makeSignalShapesMVV("JJ_XqZ",qZTemplate)
 makeSignalShapesMJJ("JJ_XqZ",qZTemplate)
 makeSignalYields("JJ_XqZ",qZTemplate,BRqW,{'HP':1.03,'LP':0.95})
+
+makeBackgroundShapesMJJ("nonRes","JJ",nonResTemplate,cuts['nonres'])
+makeBackgroundShapesMVVConditional("nonRes","JJ",nonResTemplate,cuts['nonres'])
